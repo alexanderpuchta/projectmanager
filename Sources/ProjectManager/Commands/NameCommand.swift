0e52d68f8e94ab2bd7b034ac81bf24367c231ccf
Alexander Puchta <alexander.puchta@appsfactory.de>
2023-03-03 20:01:13 +0100

    [ADD] #PM-4 - inputs with argument parser

---
+++ Sources/ProjectManager/Commands/NameCommand.swift
@@ -0,0 +1,21 @@
+import ArgumentParser
+
+struct Name: ParsableCommand {
+    
+    static let configuration = CommandConfiguration(abstract: "configure the name of your project")
+    
+    @Argument(help: "the name of your project")
+    private var title: String
+    
+    @Argument(help: "the clients name for file header.")
+    private var client: String
+
+        
+    // MARK: - Run
+    
+    func run() throws {
+        
+        print("Name: \(self.title)")
+        print("client: \(self.client)")
+    }
+}
